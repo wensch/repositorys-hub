4edba04e712d0aa8489ae9c8fa554a5f45184689
Mateus Wensch <wensch22@gmail.com>
2025-01-13 15:02:40 -0300

    Details Page

--- repository-hub/src/pages/Repositorys/index.tsx
+++ repository-hub/src/pages/Repositorys/index.tsx
@@ -1,15 +1,43 @@
 
 import { useParams  } from "react-router-dom";
-
+import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+// {decodeURIComponent(repositorio)}
 const Repositorys = () => {
-
   const {repositorio} = useParams();
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect( () => {
+    setLoading(true);
+
+    async function getRepositorio() {
+
+      const [repositoryData, issuesData] = await Promise.all([
+        api.get(`repos/${repositorio}`),
+        api.get(`repos/${repositorio}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepository(repositoryData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    getRepositorio();
+  }, [repositorio])
   
   return (
-    <h1>
-      Repository
-      <span>{decodeURIComponent(repositorio)}</span> 
-    </h1>
+    <Container>
+
+    </Container>
   )
 }
 
